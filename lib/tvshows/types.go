352d5241f1d147e7755a1c9e9fbe8e8ce08cfe38
alagha-go <abdihakimosman1480@gmail.com>
2022-05-12 19:30:15 +0000

    created needed types the Tvshows packages

---
+++ lib/tvshows/types.go
@@ -0,0 +1,69 @@
+package tvshows
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+
+type Movie struct {
+	ID											primitive.ObjectID						`json:"_id,omitempty" bson:"_id,omitempty"`
+	Code										string									`json:"code,omitempty" bson:"code,omitempty"`
+	Title										string									`json:"title,omitempty" bson:"title,omitempty"`
+	Type										string									`json:"type,omitempty" bson:"type,omitempty"`
+    Available                                   bool                                    `json:"available,omitempty" bson:"available,omitempty"`
+    PageUrl										string									`json:"page_url,omitempty" bson:"page_url,omitempty"`
+    ImageUrl									string									`json:"image_url,omitempty" bson:"image_url"`
+    Uploaded                                    bool                                    `json:"uploaded,omitempty" bson:"uploaded,omitempty"`
+    Released									*time.Time								`json:"released,omitempty" bson:"released"`
+    Urls										[]string								`json:"urls,omitempty" bson:"urls,omitempty"`
+	Collected									bool									`json:"collected,omitempty" bson:"collected,omitempty"`
+    Genres										[]string								`json:"genre,omitempty" bson:"genre,omitempty"`
+	Server										*Server									`json:"server,omitempty" bson:"server,omitempty"`
+    Servers										[]Server								`json:"servers,omitempty" bson:"servers,omitempty"`
+    Casts										[]string								`json:"casts,omitempty" bson:"casts,omitempty"`
+    Duration									time.Duration							`json:"duration,omitempty" bson:"duration,omitempty"`
+    Countries									[]string								`json:"countries,omitempty" bson:"countries,omitempty"`
+    Producers									[]string								`json:"producers,omitempty" bson:"producers,omitempty"`
+    Description									string									`json:"description,omitempty" bson:"description,omitempty"`
+}
+
+
+type Season struct {
+	ID											primitive.ObjectID						`json:"_id,omitempty" bson:"_id,omitempty"`
+	Name										string									`json:"name,omitempty" bson:"name,omitempty"`
+    Index                                       int                                     `json:"index,omitempty" bson:"index,omitempty"`
+    Code										string									`json:"code,omitempty" bson:"code,omitempty"`
+	Episodes									[]Episode								`json:"episodes,omitempty" bson:"episodes,omitempty"`
+}
+
+
+type Episode struct {
+	ID											primitive.ObjectID						`json:"_id,omitempty" bson:"_id,omitempty"`
+	Name										string									`json:"name,omitempty" bson:"name,omitempty"`
+    Index                                       int                                     `json:"index,omitempty" bson:"index,omitempty"`
+    Available                                   bool                                    `json:"available,omitempty" bson:"available,omitempty"`
+    Code										string									`json:"code,omitempty" bson:"code,omitempty"`
+    Urls										[]string								`json:"urls,omitempty" bson:"urls,omitempty"`
+	Server										*Server									`json:"server,omitempty" bson:"server,omitempty"`
+	Servers										[]Server								`json:"servers,omitempty" bson:"servers,omitempty"`
+}
+
+
+type Server struct {
+	ID											primitive.ObjectID						`json:"_id,omitempty" bson:"_id,omitempty"`
+    Name                                        string                               	`json:"name,omitempty" bson:"name,omitempty"`
+    Id                                          string                               	`json:"id,omitempty" bson:"id,omitempty"`
+    WatchID                                     string                               	`json:"watch_id,omitempty" bson:"watch_id,omitempty"`
+    Url                                         string                               	`json:"url,omitempty" bson:"url,omitempty"`
+}
+
+
+type LinkResponse struct {
+	Type    string        `json:"type"`   
+	Link    string        `json:"link"`   
+	Sources []interface{} `json:"sources"`
+	Tracks  []interface{} `json:"tracks"` 
+	Title   string        `json:"title"`  
+}
