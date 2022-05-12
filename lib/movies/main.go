0ebb620d1f1afb950fe346610cb5a9cf779f8906
alagha-go <abdihakimosman1480@gmail.com>
2022-05-12 16:58:15 +0000

    created function to get number of pages available for all the movies

--- lib/movies/main.go
+++ lib/movies/main.go
@@ -1,6 +1,39 @@
 package movies
 
+import (
+	"strconv"
+	"strings"
+
+	"github.com/gocolly/colly"
+)
+
+
+var (
+	PagesMovies []Movie
+)
 
 func Main() {
-	
+
+}
+
+
+func GetNumberOfPages() int {
+	var err error
+	var numberofPages int
+	collector := colly.NewCollector()
+
+	collector.OnHTML(".pagination.pagination-lg.justify-content-center", func(element *colly.HTMLElement) {
+		element.ForEach(".page-item", func(_ int, element *colly.HTMLElement) {
+			title := element.ChildAttr("a", "title")
+			href := element.ChildAttr("a", "href")
+			if title == "Last" {
+				href = strings.ReplaceAll(href, "/movie?page=", "")
+				numberofPages, err = strconv.Atoi(href)
+			}
+		})
+	})
+
+	collector.Visit("https://tinyzonetv.to/movie")
+
+	return numberofPages
 }
