64841a0c4bb139a6ff8177a33685ef88aa063d83
alagha-go <abdihakimosman1480@gmail.com>
2022-05-12 17:00:01 +0000

    added Type field to the Movie struct

--- lib/movies/types.go
+++ lib/movies/types.go
@@ -11,6 +11,7 @@ type Movie struct {
 	ID											primitive.ObjectID						`json:"_id,omitempty" bson:"_id,omitempty"`
 	Code										string									`json:"code,omitempty" bson:"code,omitempty"`
 	Title										string									`json:"title,omitempty" bson:"title,omitempty"`
+	Type										string									`json:"type,omitempty" bson:"type,omitempty"`
     Available                                   bool                                    `json:"available,omitempty" bson:"available,omitempty"`
     PageUrl										string									`json:"page_url,omitempty" bson:"page_url,omitempty"`
     ImageUrl									string									`json:"image_url,omitempty" bson:"image_url"`
@@ -21,7 +22,7 @@ type Movie struct {
 	Server										*Server									`json:"server,omitempty" bson:"server,omitempty"`
     Servers										[]Server								`json:"servers,omitempty" bson:"servers,omitempty"`
     Casts										[]string								`json:"casts,omitempty" bson:"casts,omitempty"`
-    Duration									int										`json:"duration,omitempty" bson:"duration,omitempty"`
+    Duration									time.Duration										`json:"duration,omitempty" bson:"duration,omitempty"`
     Countries									[]string								`json:"countries,omitempty" bson:"countries,omitempty"`
     Producers									[]string								`json:"producers,omitempty" bson:"producers,omitempty"`
     Description									string									`json:"description,omitempty" bson:"description,omitempty"`
