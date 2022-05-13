f2b9a820a1ed3c069c9efae097c2a7934ac85ca8
alagha-go <abdihakimosman1480@gmail.com>
2022-05-13 08:31:07 +0000

    added function to get number of available pages, jsonmarshal and handleError functions

--- lib/tvshows/main.go
+++ lib/tvshows/main.go
@@ -1,5 +1,61 @@
 package tvshows
 
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/gocolly/colly"
+)
+
+var (
+	PagesTvShows []Movie
+	DBPages		[]Movie
+	TvShows		[]Movie
+)
+
 func Main() {
-	
+	LoadDBPages()
+	CollectPages(GetNumberOfPages())
+	CollectAllTvShows()
+}
+
+func GetNumberOfPages() int {
+	var err error
+	var numberofPages int
+	collector := colly.NewCollector()
+
+	collector.OnHTML(".pagination.pagination-lg.justify-content-center", func(element *colly.HTMLElement) {
+		element.ForEach(".page-item", func(_ int, element *colly.HTMLElement) {
+			title := element.ChildAttr("a", "title")
+			href := element.ChildAttr("a", "href")
+			if title == "Last" {
+				href = strings.ReplaceAll(href, "/movie?page=", "")
+				numberofPages, err = strconv.Atoi(href)
+				HanleError(err)
+			}
+		})
+	})
+
+	collector.Visit("https://tinyzonetv.to/movie")
+
+	return numberofPages
+}
+
+
+func JsonMarshal(data interface{}) []byte {
+	var buff bytes.Buffer
+	encoder := json.NewEncoder(&buff)
+	encoder.SetEscapeHTML(false)
+	encoder.Encode(data)
+	return buff.Bytes()
+}
+
+
+func HanleError(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
 }
