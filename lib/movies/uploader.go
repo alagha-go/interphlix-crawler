d1b10270dcce03a7653b7e5ca5aa446da4939940
alagha-go <abdihakimosman1480@gmail.com>
2022-05-13 17:14:10 +0000

    created function to reupload unUploaded Movies

---
+++ lib/movies/uploader.go
@@ -0,0 +1,26 @@
+package movies
+
+import (
+	"encoding/json"
+	"io/ioutil"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+
+func UploadUnUploadedMovies() {
+	var Movies []Movie
+	data, err := ioutil.ReadFile("./DB/Movies/movies.json")
+	HanleError(err)
+	json.Unmarshal(data, &Movies)
+	for index := range Movies {
+		if !Movies[index].Uploaded {
+			Movies[index].Upload()
+			if !Movies[index].Uploaded {
+				Movies[index].ID = primitive.NewObjectID()
+				Movies[index].Upload()
+			}
+			ioutil.WriteFile("./DB/Movies/movies.json", JsonMarshal(Movies), 0755)
+		}
+	}
+}
