7162922be2911329a3121236559c98d79f937f32
alagha-go <abdihakimosman1480@gmail.com>
2022-05-13 17:42:40 +0000

    added function to load Saved Movies

--- lib/movies/pages.go
+++ lib/movies/pages.go
@@ -78,3 +78,9 @@ func SavePagesData() {
 	data := JsonMarshal(PagesMovies)
 	ioutil.WriteFile("./DB/Movies/pages.json", data, 0755)
 }
+
+func LoadDBMovies() {
+	data, err := ioutil.ReadFile("./DB/Movies/movies.json")
+	HanleError(err)
+	json.Unmarshal(data, &DBMovies)
+}
