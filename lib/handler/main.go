f9d40d9ea964b56cbe5c332491f6d88dda91785c
alagha-go <abdihakimosman1480@gmail.com>
2022-05-14 07:56:54 +0000

    added function to handle and server the crawler statistics

--- lib/handler/main.go
+++ lib/handler/main.go
@@ -1,6 +1,11 @@
 package handler
 
-import "net/http"
+import (
+	"crawler/lib/movies"
+	"crawler/lib/tvshows"
+	"encoding/json"
+	"net/http"
+)
 
 
 func Main() {
@@ -8,5 +13,9 @@ func Main() {
 }
 
 func GetStats(res http.ResponseWriter, req *http.Request) {
-	
+	MovieType := Stats{"Movie", movies.LoopNumber, movies.PagesPosition, movies.MoviesPosition, movies.Available, movies.MoviesPosition-movies.Available, movies.UploadedMovies, movies.MoviesPosition-movies.UploadedMovies}
+	TvShowType := Stats{"Tv-Show", tvshows.LoopNumber, tvshows.PagesPosition, tvshows.MoviesPosition, tvshows.Available, tvshows.MoviesPosition-tvshows.Available, tvshows.UploadedMovies, tvshows.MoviesPosition-tvshows.UploadedMovies}
+	var StatsData = []Stats{MovieType, TvShowType}
+	res.WriteHeader(200)
+	json.NewEncoder(res).Encode(StatsData)
 }
