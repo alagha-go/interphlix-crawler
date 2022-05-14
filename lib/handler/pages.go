ded4d8e6a152e3241ffb8f084228880c9a7c9a04
alagha-go <abdihakimosman1480@gmail.com>
2022-05-14 08:16:24 +0000

    added function handle request to get pages of Movies

---
+++ lib/handler/pages.go
@@ -0,0 +1,13 @@
+package handler
+
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+func MoviePages(res http.ResponseWriter, req *http.Request) {
+	data, err := ioutil.ReadFile("./DB/Movies/pages.json")
+	HandleError(err)
+	res.WriteHeader(200)
+	res.Write(data)
+}
