decda39358b29a8f7e8ca0afcbef6e3a0c125292
alagha-go <abdihakimosman1480@gmail.com>
2022-05-29 08:05:20 +0000

    changed struct Stats to Statistics

--- lib/handler/structs.go
+++ lib/handler/structs.go
@@ -1,10 +1,13 @@
 package handler
 
-type Stats struct {
-	Type									string								`json:"type,omitempty"`
-	LoopNumber								int									`json:"loop-number,omitempty"`
-	PagesPosition							int									`json:"pages-position,omitempty"`
-	Position								int									`json:"position,omitempty"`
+type Statistics struct {
+	Type									string						`json:"type,omitempty"`
+	LoopNumber								int							`json:"loop-number,omitempty"`
+	PagesLength								int							`json:"pages-length,omitempty"`
+	CurrentPageNumber						int							`json:"current-page,omitempty"`
+	CurrentPageCollectedMovies				int							`json:"page-collected-movies,omitempty"`
+	TotalNumberOfMovies						int							`json:"total-movies,omitempty"`
+	CurrentMovie							int							`json:"current-movie,omitempty"`
 	Available								int									`json:"available,omitempty"`
 	UnAvailable								int									`json:"unavailable,omitempty"`
 	Uploaded								int									`json:"uploaded,omitempty"`
